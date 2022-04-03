a93cdfb1c9a46e977ca055176a4dd8202e83d0dd
Thomas Leung <thomasleung9912@gmail.com>
2022-04-03 16:34:06 +0100

    register

--- blog.sql
+++ blog.sql
@@ -3,13 +3,6 @@ CREATE DATABASE blog-website
 create schema blog-website;
 set schema 'blog-website';
 
---if recreating the database, need to drop tables in this order to avoid constraint violations
---drop table roombooking;
---drop table booking;
---drop table customer;
---drop table room;
---drop table rates;
-
 create table user (
 	user_id integer unique not null,
 	username varchar(80) not null,
