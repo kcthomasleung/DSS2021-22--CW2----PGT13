c13322b972bd9b80eb30c6263965a8f3eeae676b
Thomas Leung <thomasleung9912@gmail.com>
2022-04-05 13:48:30 +0100

    insert user to database

--- public/js/register.js
+++ public/js/register.js
@@ -1,16 +1,3 @@
 const register_form = document.querySelector("#register_form");
 
 
-function signup(email, password) {
-    const salt = randomBytes(16).toString('hex');
-    const hashedPassword = hash(password + salt);
-    return {
-        email,
-        salt,
-        hashedPassword
-    };
-}
-
-function hash(input) {
-    return createHash('sha256').update(input).digest('hex');
-}
