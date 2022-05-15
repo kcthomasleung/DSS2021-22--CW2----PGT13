043823c1383e1d47051d5ed9ed1700927b44c860
Deep52 <pardeep.nsr@gmail.com>
2022-05-15 15:21:22 +0100

    header

--- public/js/register.js
+++ public/js/register.js
@@ -1,13 +1,12 @@
 function validate_password() {
- 
+
     const password = document.getElementById('password').value;
     const password_confirm = document.getElementById('password_confirm').value;
 
     // check if passwords match
     if (password != password_confirm) {
         document.getElementById('incorrect_password').style.color = 'red';
-        document.getElementById('incorrect_password').innerHTML
-          = 'Please enter the same password';
+        document.getElementById('incorrect_password').innerHTML = 'Please enter the same password';
         document.getElementById('submit_button').disabled = true;
         document.getElementById('submit_button').style.opacity = (0.4);
     } else {
@@ -20,3 +19,21 @@ function validate_password() {
 }
 
 document.getElementById('password_confirm').addEventListener('keyup', validate_password);
+
+
+const button = document.getElementById('search');
+button.addEventListener('keyup', function(e) {
+    console.log('button was clicked');
+
+    fetch('/clicked', { method: 'POST' })
+        .then(function(response) {
+            if (response.ok) {
+                console.log('Click was recorded');
+                return;
+            }
+            throw new Error('Request failed.');
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+});
